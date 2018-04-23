e723f632740fd58e97c166562ae15f8acca46e60
zengzhishi <zengzhishi@sbrella.com>
2018-04-23 19:50:32 +0800

    Fix some code format problem

--- pymake/builtins.py
+++ pymake/builtins.py
@@ -2,65 +2,68 @@
 import errno, sys, os, shutil, time
 from getopt import getopt, GetoptError
 
-from pymake  import errors
+from pymake import errors
 
 __all__ = ["mkdir", "rm", "sleep", "touch"]
 
+
 def mkdir(args):
-  """
-  Emulate some of the behavior of mkdir(1).
-  Only supports the -p (--parents) argument.
-  """
-  try:
-    opts, args = getopt(args, "p", ["parents"])
-  except GetoptError as e:
-    raise errors.PythonError("mkdir: %s" % e, 1)
-  parents = False
-  for o, a in opts:
-    if o in ('-p', '--parents'):
-      parents = True
-  for f in args:
+    """
+    Emulate some of the behavior of mkdir(1).
+    Only supports the -p (--parents) argument.
+    """
     try:
-      if parents:
-        os.makedirs(f)
-      else:
-        os.mkdir(f)
-    except OSError as e:
-      if e.errno == errno.EEXIST and parents:
-        pass
-      else:
+        opts, args = getopt(args, "p", ["parents"])
+    except GetoptError as e:
         raise errors.PythonError("mkdir: %s" % e, 1)
+    parents = False
+    for o, a in opts:
+        if o in ('-p', '--parents'):
+            parents = True
+    for f in args:
+        try:
+            if parents:
+                os.makedirs(f)
+            else:
+                os.mkdir(f)
+        except OSError as e:
+            if e.errno == errno.EEXIST and parents:
+                pass
+            else:
+                raise errors.PythonError("mkdir: %s" % e, 1)
+
 
 def rm(args):
-  """
-  Emulate most of the behavior of rm(1).
-  Only supports the -r (--recursive) and -f (--force) arguments.
-  """
-  try:
-    opts, args = getopt(args, "rRf", ["force", "recursive"])
-  except GetoptError as e:
-    raise errors.PythonError("rm: %s" % e, 1)
-  force = False
-  recursive = False
-  for o, a in opts:
-    if o in ('-f', '--force'):
-      force = True
-    elif o in ('-r', '-R', '--recursive'):
-      recursive = True
-  for f in args:
-    if os.path.isdir(f):
-      if not recursive:
-        raise errors.PythonError("rm: cannot remove '%s': Is a directory" % f, 1)
-      else:
-        shutil.rmtree(f, force)
-    elif os.path.exists(f):
-      try:
-        os.unlink(f)
-      except:
-        if not force:
-          raise errors.PythonError("rm: failed to remove '%s': %s" % (f, sys.exc_info()[0]), 1)
-    elif not force:
-      raise errors.PythonError("rm: cannot remove '%s': No such file or directory" % f, 1)
+    """
+    mulate most of the behavior of rm(1).
+    Only supports the -r (--recursive) and -f (--force) arguments.
+    """
+    try:
+        opts, args = getopt(args, "rRf", ["force", "recursive"])
+    except GetoptError as e:
+        raise errors.PythonError("rm: %s" % e, 1)
+    force = False
+    recursive = False
+    for o, a in opts:
+        if o in ('-f', '--force'):
+            force = True
+        elif o in ('-r', '-R', '--recursive'):
+            recursive = True
+    for f in args:
+        if os.path.isdir(f):
+            if not recursive:
+                raise errors.PythonError("rm: cannot remove '%s': Is a directory" % f, 1)
+            else:
+                shutil.rmtree(f, force)
+        elif os.path.exists(f):
+            try:
+                os.unlink(f)
+            except:
+                if not force:
+                    raise errors.PythonError("rm: failed to remove '%s': %s" % (f, sys.exc_info()[0]), 1)
+                elif not force:
+                    raise errors.PythonError("rm: cannot remove '%s': No such file or directory" % f, 1)
+
 
 def sleep(args):
     """
@@ -82,6 +85,7 @@ def sleep(args):
             raise errors.PythonError("sleep: invalid time interval '%s'" % a, 1)
     time.sleep(total)
 
+
 def touch(args):
     """
     Emulate the behavior of touch(1).
@@ -98,6 +102,7 @@ def touch(args):
         m = re.match('^(?P<Y>(?:\d\d)?\d\d)?(?P<M>\d\d)(?P<D>\d\d)(?P<h>\d\d)(?P<m>\d\d)(?:\.(?P<s>\d\d))?$', opts['-t'])
         if not m:
             raise errors.PythonError("touch: invalid date format '%s'" % opts['-t'], 1)
+
         def normalized_field(m, f):
             if f == 'Y':
                 if m.group(f) is None:
@@ -111,6 +116,7 @@ def touch(args):
             if m.group(f) is None:
                 return localtime()[0] if f == 'Y' else 0
             return int(m.group(f))
+
         time = [normalized_field(m, f) for f in ['Y', 'M', 'D', 'h', 'm', 's']] + [0, 0, -1]
         time = mktime(time)
         times = (time, time)
